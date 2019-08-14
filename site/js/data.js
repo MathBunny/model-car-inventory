3035f07ee69353da86876c382e6a0e5f7aa394dd
MathBunny <horatiulazu@gmail.com>
2019-08-14 11:02:44 -0400

    Add React components for search bar

--- site/js/data.js
+++ site/js/data.js
@@ -1,4 +1,10 @@
 
 function getCars() {
-    
+
+}
+
+function capitalizeFirstLetter(string) {
+    if (string === undefined)
+        return;
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
