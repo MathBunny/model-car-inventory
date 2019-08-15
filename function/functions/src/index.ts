53c0ab267f4b69f0788a83fe46b453aba8329cf2
MathBunny <horatiulazu@gmail.com>
2019-08-15 17:39:33 -0400

    Add function for retrieving firebase data

--- function/functions/src/index.ts
+++ function/functions/src/index.ts
@@ -1,8 +1,33 @@
-import * as functions from 'firebase-functions';
+const firebase = require('firebase-admin');
+const functions = require('firebase-functions');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Simulator link:
+// http://localhost:5000/toy-car-visualizer/us-central1/retrieveModels
+
+export const retrieveModels = functions.https.onRequest((request: any, response: any) => {
+    const firebaseConfig = {
+      authDomain: 'toy-car-visualizer.firebaseapp.com',
+      databaseURL: 'https://toy-car-visualizer.firebaseio.com',
+      projectId: 'toy-car-visualizer',
+      storageBucket: '',
+      messagingSenderId: '30835560445',
+      appId: '1:30835560445:web:e7fa1a767bbebdce'
+    };
+        
+    firebase.initializeApp(firebaseConfig);
+    const db = firebase.firestore();
+
+    const res: any[] = [];
+
+    db.collection("cars").get().then((querySnapshot: any) => {
+        querySnapshot.forEach((doc:any) => {
+            res.push(doc.data());
+        });
+
+        console.log('Successfully retrieved!');
+        response.send( {'data': res} );
+    }).catch((e: any) => {
+        console.log('Promise Rejected ' + e);
+        response.send( {'data': []} );
+    });
+});
